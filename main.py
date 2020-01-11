255167786df3d25fb195eb179c2c159b2473b1a7
maxijer1234@yandex.ru <maxijer1234>
2020-01-11 17:10:02 +0300

    add move and stop

--- main.py
+++ main.py
@@ -27,18 +27,9 @@ class Board:
         self.top = top
         self.cell_size = cell_size
 
-    def render(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                pygame.draw.rect(screen, pygame.Color('white'),
-                                 (x * self.cell_size + self.left,
-                                  y * self.cell_size + self.top,
-                                  self.cell_size, self.cell_size),
-                                 1)
-
     def get_cell(self, mouse_pos):
-        celi_x = mouse_pos[0] // (self.cell_size + self.left)
-        celi_y = mouse_pos[1] // (self.cell_size + self.top)
+        celi_x = mouse_pos[0] // self.cell_size
+        celi_y = mouse_pos[1] // self.cell_size
         if not (
                 celi_x < 0 or celi_x >= self.width or celi_y < 0 or celi_y >= self.height):
             return celi_y, celi_x
@@ -64,27 +55,40 @@ class T:
     def __init__(self):
         global board
         self.chast = list()
-        self.x = 0
+        self.x = 210
         self.y = 0
+        self.flag = True
 
     def create_shape(self):
         z = load_image('tetris.png', -1)
-        first_coord = (210, self.y + 18)
-        screen.blit(z, (210, self.y + 18))
-        self.add_in_board(first_coord)
-        second_coord = (210, self.y + 48)
+        first_coord = (self.x, self.y + 48)
+        fir = self.check_coord(first_coord)
+        screen.blit(z, first_coord)
+        second_coord = (self.x, self.y + 78)
+        sec = self.check_coord(second_coord)
         screen.blit(z, second_coord)
-        self.add_in_board(second_coord)
-        three_cord = (210, self.y + 78)
+        three_cord = (self.x, self.y + 108)
+        three = self.check_coord(three_cord)
         screen.blit(z, three_cord)
-        self.add_in_board(three_cord)
-        four_coord = (240, self.y + 48)
-        screen.blit(z, (240, self.y + 48))
-        self.add_in_board(four_coord)
+        four_coord = (self.x + 30, self.y + 78)
+        four = self.check_coord(four_coord)
+        screen.blit(z, four_coord)
+        clock.tick(2)
+        if three[0] == 19 or board.board[three[1]][three[0] + 1] == 1 or board.board[four[0]] == 0 and \
+                board.board[four[0] + 1] == 1:
+            self.add_in_board(fir)
+            self.add_in_board(sec)
+            self.flag = False
+            self.add_in_board(three)
+            self.add_in_board(four)
+
+    def check_coord(self, coord):
+        f = board.get_cell((coord[0], coord[1]))
+        return f
 
     def add_in_board(self, coord):
-        f = board.get_cell((coord[0], coord[1]))
-        board.board[f[0]][f[1]] = 1
+        board.board[coord[0]][coord[1]] = 1
+        print(board.board)
 
 
 running = True
@@ -93,11 +97,19 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill((0, 0, 0))
-    f.y += 30
-    clock.tick(2)
-    f.create_shape()
-    board.render()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                f.x += 30
+            elif event.key == pygame.K_LEFT:
+                f.x -= 30
+            elif event.key == pygame.K_DOWN:
+                f.y += 30
+    if f.flag:
+        screen.fill((0, 0, 0))
+        f.create_shape()
+        screen.fill((0, 0, 0))
+        f.y += 30
+        f.create_shape()
+        clock.tick(2)
+        pygame.display.flip()
     pygame.display.flip()
-pygame.display.flip()
