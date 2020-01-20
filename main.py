66c94a8c49bbc1e72daad371a209f552cdb34df2
maxijer1234@yandex.ru <maxijer1234>
2020-01-20 22:45:53 +0300

    add on board and random shape

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Name(object):
     MirroredLShape = 7
 
 
-class Shape(object):
+class Figura(object):
     coordsTable = (
         ((0, 0), (0, 0), (0, 0), (0, 0)),
         ((0, -1), (0, 0), (-1, 0), (-1, 1)),
@@ -34,6 +34,26 @@ class Shape(object):
         ((-1, -1), (0, -1), (0, 0), (0, 1)),
         ((1, -1), (0, -1), (0, 0), (0, 1))
     )
+
+    def __init__(self):
+
+        self.coords = [[0, 0] for i in range(4)]
+        self.pieceShape = Name.NoShape
+
+        self.postavit_figuru(Name.NoShape)
+
+    def postavit_figuru(self, shape):  # метод для поставки фигуры на доске
+
+        table = Figura.coordsTable[shape]
+
+        for i in range(4):
+            for j in range(2):
+                self.coords[i][j] = table[i][j]
+
+        self.pieceShape = shape
+
+    def setRandomfigura(self):  # выбираем рандомную фигуру
+        self.postavit_figuru(random.randint(1, 7))
 
 
 running = True
