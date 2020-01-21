a751e33a387cd6572f0375170fcff8f845a03566
maxijer1234@yandex.ru <maxijer1234>
2020-01-21 15:16:16 +0300

    pochti vse

--- norm.py
+++ norm.py
@@ -6,9 +6,46 @@ pygame.init()
 
 width, height = (700, 700)
 size = width, height
+fps = 5
 # screen — холст, на котором нужно рисовать:
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+
+score = 0
+
+
+def check():
+    global score, fps
+    spi = list()
+    flag = False
+    for i in range(len(board.board)):
+        for j in board.board[i]:
+            if j != '0':
+                spi.append(True)
+            else:
+                spi.append(False)
+        if all(spi):
+            for j in board.board[:i]:
+                board.board[j + 1] = board.board[j]
+            score += 1
+            flag = True
+    if score == 0:
+        fps = 5
+    elif score == 5:
+        fps = 10
+    elif score == 7:
+        fps = 15
+    if flag:
+        return True
+    return False
+
+
+
+def game_over():
+    for i in board.board[5]:
+        if i != '0':
+            return True
+    return False
 
 
 class Board:
@@ -70,10 +107,14 @@ class O:
         self.glav = 'create_shape'
         self.func = ['create_shape']
         self.flag = True
-        color = random.choice(['red', 'blue'])
+        color = random.choice(['red', 'blue', 'yellow', 'green'])
         self.color = color
         if color == 'red':
-            self.z = load_image('red.png', -1)
+            self.z = load_image('tetris2.png', -1)
+        elif color == 'yellow':
+            self.z = load_image('tetris1.png', -1)
+        elif color == 'grenn':
+            self.z = load_image('tetris3.png', -1)
         else:
             self.z = load_image('tetris.png', -1)
 
@@ -165,6 +206,118 @@ class O:
                         f.y += 30
 
 
+class I:
+    def __init__(self):
+        global board, game1
+        self.chast = list()
+        self.x = 210
+        self.y = 0
+        self.glav = 'create_shape'
+        self.func = ['create_shape', 'second']
+        self.flag = True
+        color = random.choice(['red', 'blue', 'yellow', 'green'])
+        self.color = color
+        if color == 'red':
+            self.z = load_image('tetris2.png', -1)
+        elif color == 'yellow':
+            self.z = load_image('tetris1.png', -1)
+        elif color == 'grenn':
+            self.z = load_image('tetris3.png', -1)
+        else:
+            self.z = load_image('tetris.png', -1)
+
+    def create_shape(self):
+        first_coord = (self.x, self.y + 48)
+        second_coord = (self.x, self.y + 78)
+        three_cord = (self.x, self.y + 108)
+        four_coord = (self.x, self.y + 138)
+        self.master_shape(first_coord, second_coord, three_cord, four_coord, 1)
+
+    def master_shape(self, first_coord, second_coord, three_coord,
+                     four_coord, shape):  # оптимизация и расстановка фигур
+        if shape == 1:
+            self.krai_up = self.check_coord(first_coord)
+            self.krai_right = self.check_coord(second_coord)
+            self.krai_left = self.check_coord(three_coord)
+            self.krai_down = self.check_coord(four_coord)
+            screen.blit(self.z, first_coord)
+            screen.blit(self.z, second_coord)
+            screen.blit(self.z, three_coord)
+            screen.blit(self.z, four_coord)
+            if self.krai_down[0] == 19 or board.board[self.krai_down[0] + 1][self.krai_down[1]] != '0':
+                self.no_problen(self.krai_up, first_coord, self.krai_right, second_coord,
+                                self.krai_left,
+                                three_coord, self.krai_down,
+                                four_coord)
+        elif shape == 2:
+            self.krai_up = self.check_coord(second_coord)
+            self.krai_right = self.check_coord(four_coord)
+            self.krai_left = self.check_coord(first_coord)
+            self.krai_down = self.check_coord(three_coord)
+            screen.blit(self.z, first_coord
+                        )
+            screen.blit(self.z, second_coord)
+            screen.blit(self.z, three_coord)
+            screen.blit(self.z, four_coord)
+            if self.krai_down[0] == 19 or board.board[self.krai_down[0] + 1][
+                self.krai_down[1]] != '0' or board.board[self.krai_left[0] + 1][
+                self.krai_left[1]] != '0' or board.board[self.krai_right[0] + 1][
+                self.krai_right[1]] != '0':
+                self.no_problen(self.krai_up, first_coord, self.krai_right, second_coord,
+                                self.krai_left,
+                                three_coord, self.krai_down,
+                                four_coord)
+
+    def no_problen(self, fir, first_coord, sec, second_coord, three,
+                   three_coord, four,
+                   four_coord):  # добавление на доску , сокращение copy paste
+        self.add_in_board(fir, first_coord)
+        self.add_in_board(sec, second_coord)
+        self.flag = False
+        self.add_in_board(three, three_coord)
+        self.add_in_board(four, four_coord)
+
+    def check_coord(self, coord):
+        f = board.get_cell((coord[0], coord[1]))
+        return f
+
+    def add_in_board(self, coord, vse):
+        board.board[coord[0]][coord[1]] = (vse, self.color)
+
+    def second(self):
+        first_coord = (self.x + 30, self.y + 48)
+        second_coord = (self.x + 60, self.y + 48)
+        three_cord = (self.x + 90, self.y + 48)
+        four_coord = (self.x + 120, self.y + 48)
+        self.master_shape(first_coord, second_coord, three_cord, four_coord, 2)
+
+    def game(self, position):
+        global q
+        if position == 1:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if move(f, 'left'):
+                        f.x -= 30
+                elif event.key == pygame.K_RIGHT:
+                    if move(f, 'right'):
+                        f.x += 30
+                elif event.key == pygame.K_DOWN:
+                    if move(f, 'down'):
+                        f.y += 30
+        elif position == 2:
+            global game1
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if move(f, 'left'):
+                        f.x -= 30
+                elif event.key == pygame.K_RIGHT:
+                    if move(f, 'right'):
+                        f.x += 30
+                elif event.key == pygame.K_DOWN:
+                    if move(f, 'down'):
+                        f.y += 30
+
+
 class S:
     def __init__(self):
         global board
@@ -177,8 +330,14 @@ class S:
         self.flag = True
         color = random.choice(['red', 'blue'])
         self.color = color
+        color = random.choice(['red', 'blue', 'yellow', 'green'])
+        self.color = color
         if color == 'red':
-            self.z = load_image('red.png', -1)
+            self.z = load_image('tetris2.png', -1)
+        elif color == 'yellow':
+            self.z = load_image('tetris1.png', -1)
+        elif color == 'grenn':
+            self.z = load_image('tetris3.png', -1)
         else:
             self.z = load_image('tetris.png', -1)
 
@@ -291,7 +450,6 @@ def move(f, hod):
     return False
 
 
-
 class T:
     def __init__(self):
         global board
@@ -303,10 +461,14 @@ class T:
         self.func = ['create_shape', 'second', 'three', 'four']
         self.flag = True
         self.flag_create = False
-        color = random.choice(['red', 'blue'])
+        color = random.choice(['red', 'blue', 'yellow', 'green'])
         self.color = color
         if color == 'red':
-            self.z = load_image('red.png', -1)
+            self.z = load_image('tetris2.png', -1)
+        elif color == 'yellow':
+            self.z = load_image('tetris1.png', -1)
+        elif color == 'grenn':
+            self.z = load_image('tetris3.png', -1)
         else:
             self.z = load_image('tetris.png', -1)
 
@@ -523,7 +685,7 @@ def generate(ob):
         game1 = random.choice([1, 2])
 
 
-al = [(T(), 'T'), (O(), 'O'), (S(), 'S')]
+al = [(T(), 'T'), (O(), 'O'), (S(), 'S'), (I(), 'I')]
 running = True
 glav = ['create_shape', 'second', 'three', 'four']
 f = random.choices(al)
@@ -537,6 +699,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         f.game(game1)
+        if game_over():
+            running = False
 
     if f.flag:
         screen.fill((0, 0, 0))
@@ -549,22 +713,31 @@ while running:
                         per = board.board[i][j][0][1]
                         screen.blit(image, (r, per))
                     elif board.board[i][j][1] == 'red':
-                        image = load_image('red.png', -1)
+                        image = load_image('tetris2.png', -1)
+                        r = board.board[i][j][0][0]
+                        per = board.board[i][j][0][1]
+                        screen.blit(image, (r, per))
+                    elif board.board[i][j][1] == 'yellow':
+                        image = load_image('tetris1.png', -1)
+                        r = board.board[i][j][0][0]
+                        per = board.board[i][j][0][1]
+                        screen.blit(image, (r, per))
+                    elif board.board[i][j][1] == 'green':
+                        image = load_image('tetris3.png', -1)
                         r = board.board[i][j][0][0]
                         per = board.board[i][j][0][1]
                         screen.blit(image, (r, per))
 
         f.y += 30
         exec(q)
-        clock.tick(5)
-        board.render()
+        clock.tick(fps)
         pygame.display.flip()
     else:
         go_flag = True
     if go_flag:
-        al = [(T(), 'T'), (O(), 'O'), (S(), 'S')]
+        print(board.board)
+        al = [(T(), 'T'), (O(), 'O'), (S(), 'S'), (I(), 'I')]
         f = random.choice(al)
-        print(f)
         go_flag = False
         generate(f[1])
         su = glav[game1 - 1]
